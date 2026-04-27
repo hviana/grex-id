42f278c13378d025f8b2b42d0c23fe9ab2609f43
hviana <hv5088@gmail.com>
2026-04-27 12:51:08 -0300

    fixes

--- src/contracts/tenant.ts
+++ src/contracts/tenant.ts
@@ -1,12 +1,9 @@
-export type TenantActorType = "user" | "api_token";
-
 export interface Tenant {
   id?: string;
   systemId?: string;
   companyId?: string;
-  systemSlug?: string;
-  actorType?: TenantActorType;
   actorId?: string;
-  exchangeable?: boolean;
-  exp?: number;
+  isOwner?: boolean;
+  createdAt: string;
+  updatedAt: string;
 }
